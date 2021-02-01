5dac71d42c45b4fdb66c5c55db14377abb8c4ea5
Sergio Romero <ing.isc.srl@gmail.com>
2021-02-01 14:20:02 -0600

    Code reformatted

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -2,10 +2,38 @@ import React, { useState } from "react";
 import "./Projects.style.scss";
 import back from "../../assets/back.png";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
-import { projects_data } from "./constants";
 
 const Projects = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const projects_data = [
+    {
+      name: "Adivina Quien",
+      description: "Juego clasico de adivina quien con profes",
+      repoURL: "#",
+    },
+    {
+      name: "Malla docente (Backend)",
+      description:
+        "Proyecto de la parte del servidor de gestion de cursos de docentes",
+      repoURL: "#",
+    },
+    {
+      name: "Expediente clinico",
+      description: "Proyecto que gestiona expedientes clinicos",
+      repoURL: "#",
+    },
+    {
+      name: "Agenda de citas medicas",
+      description:
+        "Proyecto de la parte del cliente para la gestion de citas medicas",
+      repoURL: "#",
+    },
+    {
+      name: "Portafolio de proyectos",
+      description: "Portafolio que kreisi xdxd",
+      repoURL: "https://github.com/Sergio-RL/portfolio-sergio-romero",
+    },
+  ];
 
   window.addEventListener("resize", () => {
     setWindowWidth(window.innerWidth);
